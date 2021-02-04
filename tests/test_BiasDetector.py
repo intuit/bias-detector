46b63bddc2035358e80ab9a0cdfb2b56b1bffa9e
Elhanan Mishraky <elhanan_mishraky@intuit.com>
2021-02-04 16:10:23 +0200

    Force providing country param to get_bias_report

--- tests/test_BiasDetector.py
+++ tests/test_BiasDetector.py
@@ -25,13 +25,15 @@ class TestBiasDetector:
     def test_get_bias_report(self):
         bias_report = bias_detector.get_bias_report(first_names=first_names_mock,
                                                     last_names=last_names_mock, zip_codes=zip_codes_mock,
-                                                    y_true=y_true_mock, y_pred=y_pred_mock, privileged_race='white')
+                                                    y_true=y_true_mock, y_pred=y_pred_mock, privileged_race='white',
+                                                    country='US')
         bias_metrics_results = bias_report.bias_metrics_results
         assert bias_metrics_results.at[BiasMetric.statistical_parity.name, 'male'].get_diff() == 0.053134892796120714
         assert bias_metrics_results.at[BiasMetric.predictive_equality.name, 'white'].get_diff() == 0.016030656112268837
         assert bias_metrics_results.at[BiasMetric.equal_opportunity.name, 'white'].get_diff() == -0.00666254800184618
         assert bias_metrics_results.at[BiasMetric.statistical_parity.name, 'male'].bias_confidence.p_value == 1.0061059942609127e-07
-        bias_report = bias_detector.get_bias_report(emails=emails_mock, y_true=y_true_mock, y_pred=y_pred_mock, privileged_race='white')
+        bias_report = bias_detector.get_bias_report(emails=emails_mock, y_true=y_true_mock, y_pred=y_pred_mock,
+                                                    privileged_race='white', country='US')
         bias_metrics_results = bias_report.bias_metrics_results
         assert bias_metrics_results.at[BiasMetric.statistical_parity.name, 'male'].get_diff() == 0.048666861482969725
 
@@ -39,7 +41,7 @@ class TestBiasDetector:
         assert bias_detector.get_bias_report(first_names=first_names_mock,
                                              last_names=last_names_mock, country='FR') is None
         assert bias_detector.get_bias_report(first_names=first_names_mock,
-                                      last_names=last_names_mock, y_pred=None) is None
+                                             last_names=last_names_mock, y_pred=None, country='US') is None
 
 def test_get_first_names_p_gender_df():
     assert p_gender_given_first_name_df.at['MOSHE', 'male'] == 1.0
